7e8e0d76e6c3a64b4d0e0e15ec7480f0ae55518e
Daniel Valdivieso-M <dvaldi7@gmail.com>
2024-07-19 16:34:41 +0200

    mario moves added

--- game.js
+++ game.js
@@ -49,16 +49,65 @@ function create() {
     .setScale(0.15) //Como la imagen es muy grande se escala con esto
 
     //AÑADIMOS EL SPRITESHEET DE MARIO
-    this.add.sprite(50, 210, "mario")
+    this.mario = this.add.sprite(50, 210, "mario")
     .setOrigin(0, 1)
 
     //AÑADIMOS EL SUELO
-    this.add.tileSprite(0, config.height -16, config.width, 32, 
+    this.add.tileSprite(0, config.height -32, config.width, 32, 
         "floorbricks")
+    .setOrigin(0, 0)
+
+    //ANIMACIONES DE MARIO AL ANDAR
+    this.anims.create({
+        key: "mario-walk",
+        frames: this.anims.generateFrameNumbers(
+            "mario",
+            { start: 3, end: 2 }
+        ),
+        frameRate: 12,
+        repeat: -1 //-1 significa infinito. Para que la repita todo el rato
+    })
+
+    //ANIMACIONES DE MARIO EN ESTÁTICO
+    
+    this.anims.create({
+        key: "mario-idle",
+        frames: [{ key: "mario", frame: 0 }]
+    })
+
+    //ANIMACIONES DE MARIO AL SALTAR
+    
+    this.anims.create({
+        key: "mario-jump",
+        frames: [{ key: "mario", frame: 5 }]
+    })
+
+    //MOVIMIENTOS
+    this.keys = this.input.keyboard.createCursorKeys()/*Método para visualizar las teclas
+    en la función update*/
 
 
 }
-
 function update() {
-    console.log("update")
+    if(this.keys.left.isDown) //para que ande a la izquierda
+    {
+        this.mario.anims.play("mario-walk", true)
+        this.mario.x -= 2 
+        this.mario.flipX = true //para girar al personaje cuando va hacia atrás
+    }
+    else if (this.keys.right.isDown) //para que anda a la derecha
+    {
+        this.mario.anims.play("mario-walk", true)
+        this.mario.x += 2 
+        this.mario.flipX = false //que deje de girar el personaje al andar
+    }
+    else //si no se mueve mario
+    {
+        this.mario.anims.play("mario-idle", true)
+    }
+    if (this.keys.up.isDown) //si presionamos la tecla de arriba (que salte)
+    {
+        this.mario.y -= 5
+        this.mario.anims.play("mario-jump", true)
+    }
 }
